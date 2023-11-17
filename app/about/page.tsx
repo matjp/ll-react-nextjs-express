635f792c007bbccdfe47291e0bb178f974c3a012
Matthew J. Penwill <[email protected]>
2023-11-17 14:15:26 +1300

    Readme udates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,9 @@ export default function Page() {
         <div className="flex justify-center p-8 text-lg">
           <p>This is a minimal app implementing a simple lending library for e-books. It is implemented as a Next.js project bootstrapped with create-next-app.</p>
         </div>
+        <div className="flex justify-center p-8 text-lg">
+          <p>An Express server implements a REST API and handles DB updates. API calls are made using fetch with CSR.</p>
+        </div>
       </>                  
     )
 }
